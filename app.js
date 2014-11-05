af10b8790b2b604df480d1686c5b26a77098d895
Zach Reed <tzreedjr@gmail.com>
2014-11-04 22:45:42 -0500

    updated file paths

---
+++ app.js
@@ -0,0 +1,35 @@
+angular.module('News', []).controller('MainCtrl', ['$scope',function($scope){
+	$scope.posts = [
+    {title: 'Obama', votes: 5},
+    {title: 'Bush Jr', votes: 2},
+    {title: 'Clinton', votes: 9},
+    {title: 'Bush Sr', votes: 15},
+    {title: 'Reagan', votes: 27}
+  ];
+
+  $scope.sortOrder = [
+    {name:'Asc', key: '+votes'},
+    {name:'Desc', key: '-votes'}
+  ];
+  $scope.defaultSort = '';
+
+	$scope.addPost = function(){
+  		if(!$scope.title || $scope.title === '') { return; }
+  		$scope.posts.push({
+    		title: $scope.title,
+    		link: $scope.link,
+    		votes: 0
+  		});
+  		$scope.title = '';
+  		$scope.link = '';
+	};
+
+	$scope.incrementvotes = function(post) {
+  		post.votes += 1;
+	};
+
+	$scope.incrementDownvotes = function(post) {
+  		post.votes -= 1;
+	};
+
+}]);
